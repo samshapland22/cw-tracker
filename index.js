b49456bca77b30d96fa6679f6a72858f8cf839a7
Samantha Shapland <samshapland@gmail.com>
2022-01-27 15:36:20 -0500

    added array of fundamental slugs

--- index.js
+++ index.js
@@ -7,4 +7,35 @@ const getData = async () => {
   console.log(grr, grr.length);
 };
 
+const fundamentalSlugs = [
+  "jennys-secret-message",
+  "the-feast-of-many-beasts",
+  "sleigh-authentication",
+  "sort-and-star",
+  "reversed-words",
+  "thinkful-string-drills-quotable",
+  "remove-duplicate-words",
+  "valid-spacing",
+  "last-survivor",
+  "alphabet-war",
+  "find-the-smallest-integer-in-the-array",
+  "well-of-ideas-easy-version",
+  "sum-mixed-array",
+  "a-needle-in-the-haystack",
+  "get-planet-name-by-id",
+  "find-the-first-non-consecutive-number",
+  "count-of-positives-slash-sum-of-negatives",
+  "largest-pair-sum-in-array",
+  "opposites-attract",
+  "convert-boolean-values-to-strings-yes-or-no",
+  "sum-of-multiples",
+  "strange-mathematics",
+  "find-out-whether-the-shape-is-a-cube",
+  "twice-as-old",
+  "find-the-remainder",
+  "youre-a-square",
+  "highest-and-lowest",
+  "halving-sum",
+];
+
 getData();
